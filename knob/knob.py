2d7b4d266eda92cf51a0f74144740b790ff80f6a
Andrew M Westling <a@andrewwestling.com>
2019-08-02 00:31:23 -0400

    Test knob functions

--- knob/knob.py
+++ knob/knob.py
@@ -5,33 +5,41 @@ This is the main runner for the knob service
 import glob
 from lib.powermate import PowerMateBase, LedEvent, MAX_BRIGHTNESS
 
-class ExamplePowerMate(PowerMateBase):
+class ShairportSyncPowermate(PowerMateBase):
   def __init__(self, path):
-    super(ExamplePowerMate, self).__init__(path)
-    self._pulsing = False
+    super(ShairportSyncPowermate, self).__init__(path)
+    self._playing = True
+    self._pulsing = self._playing
     self._brightness = MAX_BRIGHTNESS
 
   def short_press(self):
-    print('Short press!')
-    self._pulsing = not self._pulsing
-    print(self._pulsing)
-    if self._pulsing:
-      return LedEvent.pulse()
-    else:
+    if self._playing:
+      print('Pause')
+      self._playing = False
       return LedEvent(brightness=self._brightness)
+    else:
+      print('Play')
+      self._playing = True
+      return LedEvent.pulse()
 
   def long_press(self):
-    print('Long press!')
+    print('Reset')
+    self._playing = False
+    return LedEvent(brightness=0)
 
   def rotate(self, rotation):
-    print('Rotate {}!'.format(rotation))
-    self._brightness = max(0, min(MAX_BRIGHTNESS, self._brightness + rotation))
-    self._pulsing = False
-    return LedEvent(brightness=self._brightness)
+    if rotation < 1:
+      print('Volume down')
+    else:
+      print('Volume up')
 
   def push_rotate(self, rotation):
-    print('Push rotate {}!'.format(rotation))
+    if rotation < 1:
+      print('Previous Track')
+    else:
+      print('Next Track')
+
 
 if __name__ == '__main__':
-  pm = ExamplePowerMate(glob.glob('/dev/input/by-id/*PowerMate*')[0])
+  pm = ShairportSyncPowermate(glob.glob('/dev/input/by-id/*PowerMate*')[0])
   pm.run()
