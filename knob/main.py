3f2b43c50d319369c8162f80685132ee5bf86118
Andrew M Westling <a@andrewwestling.com>
2019-08-01 21:22:07 -0400

    Add PowerMate base example from library to test

--- knob/main.py
+++ knob/main.py
@@ -2,12 +2,35 @@
 """
 This is the main runner for the knob service
 """
+from powermate import PowerMateBase, LedEvent, MAX_BRIGHTNESS
 
-from time import sleep
+class ExamplePowerMate(PowerMateBase):
+  def __init__(self, path):
+    super(ExamplePowerMate, self).__init__(path)
+    self._pulsing = False
+    self._brightness = MAX_BRIGHTNESS
 
-try:
-    while True:
-        print "Hello World"
-        sleep(5)
-except KeyboardInterrupt, e:
-    print "Stopping..."
+  def short_press(self):
+    print('Short press!')
+    self._pulsing = not self._pulsing
+    print(self._pulsing)
+    if self._pulsing:
+      return LedEvent.pulse()
+    else:
+      return LedEvent(brightness=self._brightness)
+
+  def long_press(self):
+    print('Long press!')
+
+  def rotate(self, rotation):
+    print('Rotate {}!'.format(rotation))
+    self._brightness = max(0, min(MAX_BRIGHTNESS, self._brightness + rotation))
+    self._pulsing = False
+    return LedEvent(brightness=self._brightness)
+
+  def push_rotate(self, rotation):
+    print('Push rotate {}!'.format(rotation))
+
+if __name__ == '__main__':
+  pm = ExamplePowerMate(glob.glob('/dev/input/by-id/*PowerMate*')[0])
+  pm.run()
