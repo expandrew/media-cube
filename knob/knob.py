33f6bafeaef50a990c1543d3af541654c7658fbe
Andrew M Westling <a@andrewwestling.com>
2019-08-03 15:17:10 -0400

    Make the knob do the thing

--- knob/knob.py
+++ knob/knob.py
@@ -3,43 +3,48 @@
 This is the main runner for the knob service
 """
 import glob
+from pydbus import SystemBus
 from lib.powermate import PowerMateBase, LedEvent, MAX_BRIGHTNESS
 
 class ShairportSyncPowermate(PowerMateBase):
   def __init__(self, path):
     super(ShairportSyncPowermate, self).__init__(path)
-    self._playing = True
+    self._playing = bus.PlayerState == 'Playing'
     self._pulsing = self._playing
     self._brightness = MAX_BRIGHTNESS
 
   def short_press(self):
-    if self._playing:
-      print('Pause')
-      self._playing = False
-      return LedEvent(brightness=self._brightness)
-    else:
-      print('Play')
-      self._playing = True
-      return LedEvent.pulse()
+    bus.PlayPause()
+    ps = bus.PlayerState
+    self._playing = ps == 'Playing'
+    print(ps)
 
-  def long_press(self):
-    print('Reset')
-    self._playing = False
-    return LedEvent(brightness=0)
+  # def long_press(self):
+  #   print('Reset')
+  #   self._playing = bus.PlayerState
+  #   return LedEvent(brightness=0)
 
   def rotate(self, rotation):
     if rotation < 1:
-      print('Volume down')
+      bus.VolumeDown()
+      print('Volume Down')
     else:
-      print('Volume up')
+      bus.VolumeUp()
+      print('Volume Up')
 
   def push_rotate(self, rotation):
     if rotation < 1:
-      print('Previous Track')
+      bus.Previous()
+      print('Previous')
     else:
-      print('Next Track')
+      bus.Next()
+      print('Next')
 
 
 if __name__ == '__main__':
+  bus = SystemBus().get('org.gnome.ShairportSync')
+  if not bus:
+    raise Exception('No shairport-sync bus was found... we gotta stop')
+
   pm = ShairportSyncPowermate(glob.glob('/dev/input/by-id/*PowerMate*')[0])
   pm.run()
