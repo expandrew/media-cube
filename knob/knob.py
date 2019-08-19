9a4fb1a67d7f0de9c597f454738f267768763b1a
Andrew M Westling <a@andrewwestling.com>
2019-08-18 20:58:56 -0400

    Update knob to connect to Spotify
    First attempt, let's see what happens

--- knob/knob.py
+++ knob/knob.py
@@ -2,54 +2,77 @@
 """
 This is the main runner for the knob service
 """
+import os
 import glob
-from pydbus import SystemBus
+import spotipy
 from lib.powermate import PowerMateBase, LedEvent, MAX_BRIGHTNESS
 
-class ShairportSyncPowermate(PowerMateBase):
+SPOTIFY_CLIENT_ID=os.environ.get('SPOTIFY_CLIENT_ID', None)
+SPOTIFY_CLIENT_SECRET=os.environ.get('SPOTIFY_CLIENT_SECRET', None)
+SPOTIFY_REDIRECT_URI=os.environ.get('SPOTIFY_REDIRECT_URI', 'http://localhost/')
+SPOTIFY_USER_NAME=os.environ.get('SPOTIFY_USER_NAME')
+
+
+class Knob(PowerMateBase):
   def __init__(self, path):
-    super(ShairportSyncPowermate, self).__init__(path)
-    self._playing = bus.PlayerState == 'Playing'
-    self._pulsing = self._playing
+    super(Knob, self).__init__(path)
+    self._playing = False
     LedEvent(brightness=128)
 
   def get_player_state(self):
-    ps = bus.PlayerState
-    self._playing = ps == 'Playing'
-    return ps
+    self._playing = not self._playing
 
   def short_press(self):
-    ps = self.get_player_state()
+    self.get_player_state()
 
-    if ps == 'Playing':
-      bus.Pause()
-      return LedEvent(brightness=128)
+    if self._playing:
+      print('Pause')
+      return LedEvent(brightness=0)
     else:
-      bus.Play()
-      return LedEvent.pulse()
+      print('Play')
+      return LedEvent(brightness=128)
 
   def long_press(self):
     print('Reset')
-    bus.Pause()
     return LedEvent(brightness=0)
 
   def rotate(self, rotation):
     if rotation < 1:
-      bus.VolumeDown()
+      print('Volume Down')
     else:
-      bus.VolumeUp()
+      print('Volume Up')
 
   def push_rotate(self, rotation):
     if rotation < 1:
-      bus.Previous()
+      print('Previous')
     else:
-      bus.Next()
+      print('Next')
 
 
 if __name__ == '__main__':
-  bus = SystemBus().get('org.gnome.ShairportSync')
-  if not bus:
-    raise Exception('No shairport-sync bus was found... we gotta stop')
+  # Get Spotify  authentication token
+  spotify_auth_token = spotipy.util.prompt_for_user_token(
+    scope='user-modify-playback-state',
+    username=SPOTIFY_USER_NAME,
+    client_id=SPOTIFY_CLIENT_ID,
+    client_secret=SPOTIFY_CLIENT_SECRET,
+    redirect_uri=SPOTIFY_REDIRECT_URI)
 
-  pm = ShairportSyncPowermate(glob.glob('/dev/input/by-id/*PowerMate*')[0])
-  pm.run()
+  if not spotify_auth_token:
+    raise Exception('Spotify authentication token wasn\'t available... we gotta stop')
+
+  # Find MediaCube in Spotify Connect devices
+  sp = spotipy.Spotify(auth=spotify_auth_token)
+  devices = sp.devices()
+  filtered = filter(lambda device: device.get(u'name') == u'MediaCube', devices) # Returns a list of objects (hopefully only one): [{}]
+  media_cube = next(iter(filtered), None) # Gets first item in list or None if doesn't exist
+
+  if not media_cube:
+    raise Exception('MediaCube wasn\'t found in Spotify Connect... we gotta stop')
+
+  # Find PowerMate
+  powermate = Knob(glob.glob('/dev/input/by-id/*PowerMate*')[0])
+
+  if not powermate:
+    raise Exception('PowerMate wasn\'t found in devices... we gotta stop')
+  powermate.run()
