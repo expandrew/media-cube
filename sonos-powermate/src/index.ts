3cf38570f9a67722299b08db29c7edd0afb90145
Andrew Westling <a@andrewwestling.com>
2020-11-24 23:55:55 -0500

    Initialize Sonos discovery and get devices

--- sonos-powermate/src/index.ts
+++ sonos-powermate/src/index.ts
@@ -8,3 +8,18 @@
  * - PowerMate LED state matches Sonos play/pause state
  * - Double-click PowerMate button toggles two preset Sonos groups (use case is "play in just this room, or play in both rooms")
  */
+import { SonosManager, SonosDevice } from '@svrooij/sonos';
+
+const manager = new SonosManager();
+
+manager.InitializeWithDiscovery(60).then(() => {
+  const MEDIA_CUBE: SonosDevice | undefined = manager.Devices.find(
+    d => d.Host === '10.0.1.16'
+  );
+  const BEDROOM: SonosDevice | undefined = manager.Devices.find(
+    d => d.Host === '10.0.1.17'
+  );
+
+  MEDIA_CUBE?.LoadDeviceData();
+  BEDROOM?.LoadDeviceData();
+});
