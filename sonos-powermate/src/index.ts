8dd560383e808c43c44c4f261b210683bcaa2e74
Andrew Westling <a@andrewwestling.com>
2020-11-24 21:04:22 -0500

    Bootstrap sonos-powermate package with tsdx

---
+++ sonos-powermate/src/index.ts
@@ -0,0 +1,10 @@
+/**
+ * sonos-powermate
+ *
+ * Control Sonos speakers with a Griffin PowerMate
+ *
+ * Features:
+ * - PowerMate inputs control play/pause/next/previous on Sonos
+ * - PowerMate LED state matches Sonos play/pause state
+ * - Double-click PowerMate button toggles two preset Sonos groups (use case is "play in just this room, or play in both rooms")
+ */
