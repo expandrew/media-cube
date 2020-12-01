590923c04d3e56ff8afaf87041cb0e43301fca39
Andrew Westling <a@andrewwestling.com>
2020-11-30 23:53:50 -0500

    Refactor naming to primary/secondary device
    This is so my device's names don't have to be hardcoded around the class

--- sonos-powermate/src/sonos.ts
+++ sonos-powermate/src/sonos.ts
@@ -3,10 +3,13 @@ import { EventEmitter } from 'events';
 
 /**
  * My Sonos devices and their IPs
+ *
+ * `PRIMARY` is the main speaker (and acts as the group coordinator when grouped)
+ * `SECONDARY` is the speaker that joins/leaves the group when `Sonos.toggleGroup()` is called
  */
 const DEVICES = {
-  MEDIA_CUBE: { name: 'Media Cube', ip: '10.0.1.16' },
-  BEDROOM: { name: 'Bedroom', ip: '10.0.1.17' },
+  PRIMARY: { name: 'Media Cube', ip: '10.0.1.16' },
+  SECONDARY: { name: 'Bedroom', ip: '10.0.1.17' },
 };
 
 /**
@@ -22,8 +25,8 @@ export const EVENTS = {
  */
 export class Sonos extends EventEmitter {
   manager: SonosManager;
-  MEDIA_CUBE: SonosDevice | undefined;
-  BEDROOM: SonosDevice | undefined;
+  PRIMARY_DEVICE: SonosDevice | undefined;
+  SECONDARY_DEVICE: SonosDevice | undefined;
   isGrouped: boolean;
   isPlaying: boolean;
 
@@ -37,28 +40,31 @@ export class Sonos extends EventEmitter {
     this.manager.InitializeWithDiscovery().then(() => {
       this.manager.Devices.forEach(d => d.LoadDeviceData());
 
-      // Set up shortcut for Media Cube
-      this.MEDIA_CUBE = this.manager.Devices.find(
-        d => d.Host === DEVICES['MEDIA_CUBE'].ip
+      // Set up shortcut for primary device
+      this.PRIMARY_DEVICE = this.manager.Devices.find(
+        d => d.Host === DEVICES['PRIMARY'].ip
       );
-      // Set up shortcut for Bedroom
-      this.BEDROOM = this.manager.Devices.find(
-        d => d.Host === DEVICES['BEDROOM'].ip
+      // Set up shortcut for secondary device
+      this.SECONDARY_DEVICE = this.manager.Devices.find(
+        d => d.Host === DEVICES['SECONDARY'].ip
       );
 
       // Get current play state and update isPlaying
-      this.MEDIA_CUBE?.AVTransportService.Events.on('serviceEvent', data => {
-        const isPlaying = data.TransportState === 'PLAYING';
+      this.PRIMARY_DEVICE?.AVTransportService.Events.on(
+        'serviceEvent',
+        data => {
+          const isPlaying = data.TransportState === 'PLAYING';
 
-        if (this.isPlaying !== isPlaying) {
-          this.isPlaying = isPlaying;
+          if (this.isPlaying !== isPlaying) {
+            this.isPlaying = isPlaying;
+          }
+
+          isPlaying ? this.emit(EVENTS.PLAYING) : this.emit(EVENTS.PAUSED);
         }
-
-        isPlaying ? this.emit(EVENTS.PLAYING) : this.emit(EVENTS.PAUSED);
-      });
+      );
 
       // Get current grouped state and update isGrouped
-      [this.MEDIA_CUBE, this.BEDROOM].forEach(d => {
+      [this.PRIMARY_DEVICE, this.SECONDARY_DEVICE].forEach(d => {
         // Check initial group name - if it includes '+ 1' ("Media Cube + 1") then devices are grouped
         this.isGrouped = d?.GroupName?.includes('+ 1') ? true : false;
 
@@ -71,17 +77,21 @@ export class Sonos extends EventEmitter {
   }
 
   /**
-   * Toggle grouping of speakers between both Media Cube and Bedroom, and just Media Cube
+   * Toggle grouping of devices
+   *
+   * This adds or removes the SECONDARY_DEVICE from the PRIMARY_DEVICE's group
+   *
+   * Also sets `isGrouped` if both devices are in the same group
    */
   toggleGroup() {
     if (this.isGrouped) {
-      this.BEDROOM?.AVTransportService.BecomeCoordinatorOfStandaloneGroup()
+      this.SECONDARY_DEVICE?.AVTransportService.BecomeCoordinatorOfStandaloneGroup()
         .then(() => (this.isGrouped = false))
         .catch(error => {
           throw new Error(error);
         });
     } else {
-      this.BEDROOM?.JoinGroup(DEVICES['MEDIA_CUBE'].name)
+      this.SECONDARY_DEVICE?.JoinGroup(this.PRIMARY_DEVICE?.Name as string)
         .then(() => (this.isGrouped = true))
         .catch(error => {
           throw new Error(error);
@@ -93,21 +103,21 @@ export class Sonos extends EventEmitter {
    * Toggle play/pause
    */
   togglePlay() {
-    this.MEDIA_CUBE?.TogglePlayback();
+    this.PRIMARY_DEVICE?.TogglePlayback();
   }
 
   /**
    * Next track
    */
   next() {
-    this.MEDIA_CUBE?.Next();
+    this.PRIMARY_DEVICE?.Next();
   }
 
   /**
    * Previous track
    */
   previous() {
-    this.MEDIA_CUBE?.Previous();
+    this.PRIMARY_DEVICE?.Previous();
   }
 
   /**
@@ -116,10 +126,10 @@ export class Sonos extends EventEmitter {
   volumeDown() {
     // WIP: This is inelegant; volume within groups gets out of sync - refactor
     if (this.isGrouped) {
-      this.MEDIA_CUBE?.SetRelativeVolume(-2);
-      this.BEDROOM?.SetRelativeVolume(-2);
+      this.PRIMARY_DEVICE?.SetRelativeVolume(-2);
+      this.SECONDARY_DEVICE?.SetRelativeVolume(-2);
     } else {
-      this.MEDIA_CUBE?.SetRelativeVolume(-2);
+      this.PRIMARY_DEVICE?.SetRelativeVolume(-2);
     }
   }
 
@@ -129,10 +139,10 @@ export class Sonos extends EventEmitter {
   volumeUp() {
     // WIP: This is inelegant; volume within groups gets out of sync - refactor
     if (this.isGrouped) {
-      this.MEDIA_CUBE?.SetRelativeVolume(2);
-      this.BEDROOM?.SetRelativeVolume(2);
+      this.PRIMARY_DEVICE?.SetRelativeVolume(2);
+      this.SECONDARY_DEVICE?.SetRelativeVolume(2);
     } else {
-      this.MEDIA_CUBE?.SetRelativeVolume(2);
+      this.PRIMARY_DEVICE?.SetRelativeVolume(2);
     }
   }
 }
