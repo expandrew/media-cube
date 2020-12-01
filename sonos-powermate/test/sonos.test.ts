1255761e66d2b89edfa23c388e8dfb6203adf385
Andrew Westling <a@andrewwestling.com>
2020-11-30 22:20:35 -0500

    Add test stubs

---
+++ sonos-powermate/test/sonos.test.ts
@@ -0,0 +1,11 @@
+// import { Sonos } from '../src/sonos';
+
+xdescribe('Sonos', () => {
+  it('creates a new Sonos', () => {
+    /**
+     * @fixme How do you mock a Sonos device?
+     */
+    // const sonos = new Sonos();
+    // expect(sonos).toBeDefined();
+  });
+});
