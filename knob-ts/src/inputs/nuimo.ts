72969cd9f3831607168d544fa79ea4c99c4317f1
Andrew Westling <a@andrewwestling.com>
2020-12-29 23:15:04 -0500

    Add "press" for Nuimo
    I literally just copied over the logic from the PowerMate and wired it into the `selectDown` and `selectUp` events from the Nuimo
    
    SINGLE_PRESS and LONG_PRESS seem to work reliably, but DOUBLE_PRESS doesn't work how I expect it to - I suspect this has something to do with how the Nuimo sends the button events - I don't think it's a latency issue because the rotation events come through very fast
    
    May need to work around using DOUBLE_PRESS for anything with Nuimo ... there are some extra things like swipes and touches (which are also unreliable) that I could probably use to substitute what wouldn've been DOUBLE_PRESS

--- knob-ts/src/inputs/nuimo.ts
+++ knob-ts/src/inputs/nuimo.ts
@@ -23,6 +23,21 @@ const setupDebug = (nuimo: Nuimo) => {
   }
 };
 
+/** Configuration for Nuimo Sensitivity */
+const SENSITIVITY = {
+  /** Number of milliseconds for button to be held to trigger a "long press" */
+  LONG_PRESS_MS: 1000,
+  /** Number of milliseconds between presses to trigger a "double press" */
+  DOUBLE_PRESS_MS: 300,
+};
+
+/** Timers for long/double press events */
+type PressTimer = {
+  timer: ReturnType<typeof setTimeout> | undefined;
+  isRunning: boolean;
+  PRESS_MS: number;
+};
+
 const manager = DeviceDiscoveryManager.defaultManager;
 
 export const startDiscovery = async (): Promise<
@@ -50,14 +65,81 @@ export const startDiscovery = async (): Promise<
 
 export class Nuimo extends EventEmitter {
   device: NuimoControlDevice | undefined;
+  isPressed: boolean;
+  longPress: PressTimer;
+  doublePress: PressTimer;
 
   constructor() {
     super();
     setupDebug(this);
 
+    this.isPressed = false;
+    this.longPress = {
+      timer: undefined,
+      isRunning: false,
+      PRESS_MS: SENSITIVITY.LONG_PRESS_MS,
+    };
+    this.doublePress = {
+      timer: undefined,
+      isRunning: false,
+      PRESS_MS: SENSITIVITY.DOUBLE_PRESS_MS,
+    };
+
     // Find and connect to the Nuimo
     startDiscovery().then(device => {
       this.device = device;
+
+      // Set up presses
+      this.device?.on('selectDown', () => this.computePress(1));
+      this.device?.on('selectUp', () => this.computePress(0));
     });
+  }
+
+  /**
+   * Compute press inputs and emit events
+   *
+   * @param pressInput - boolean 0 or 1 for "up" or "down"
+   */
+  computePress(pressInput: 0 | 1) {
+    const isPressed = Boolean(pressInput);
+    const buttonDown = isPressed; // for readability down below
+    const buttonUp = !isPressed; // for readability down below
+
+    // If there is a change from what is currently stored:
+    if (isPressed != this.isPressed) {
+      this.isPressed = isPressed;
+
+      if (buttonDown) {
+        // Start LONG_PRESS timer
+        this.longPress.isRunning = true;
+        this.longPress.timer = setTimeout(() => {
+          this.longPress.isRunning = false;
+          this.emit(EVENTS.LONG_PRESS);
+        }, this.longPress.PRESS_MS);
+      }
+
+      if (buttonUp) {
+        if (this.longPress.isRunning) {
+          // Check if we're within a DOUBLE_PRESS timeout
+          if (this.doublePress.isRunning) {
+            // If we had a second press within a DOUBLE_PRESS timeout, it's a DOUBLE_PRESS
+            this.doublePress.isRunning = false;
+            clearTimeout(this.doublePress.timer as NodeJS.Timeout);
+            this.emit(EVENTS.DOUBLE_PRESS);
+          } else {
+            // If we aren't already in a DOUBLE_PRESS timeout, set one
+            this.doublePress.isRunning = true;
+            this.doublePress.timer = setTimeout(() => {
+              // If nothing else happens before the DOUBLE_PRESS timeout finishes, it's a SINGLE_PRESS
+              this.doublePress.isRunning = false;
+              this.emit(EVENTS.SINGLE_PRESS);
+            }, this.doublePress.PRESS_MS);
+          }
+        }
+
+        // Clear LONG_PRESS timer when released
+        clearTimeout(this.longPress.timer as NodeJS.Timeout);
+      }
+    }
   }
 }
